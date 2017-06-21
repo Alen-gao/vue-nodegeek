40c88953a1301e4af5f96746a643f7374acbe874
Alen-gao <gaopu198778@163.com>
2017-06-21 17:05:25 +0800

    首页样式

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <template>
   <div id="app">
-    <img src="http://www.nodegeek.org/logo.png">
+    <div class="head top  J-Head">
+    <div class="width">
+      <a href="/" class="fl logo"><img src="http://www.nodegeek.org/logo.png"></a>
+      <div class="settings">
+        <div class="raidus">未登录</div>
+        <div class="trans">
+          <a href="/register">注册</a>
+          <a href="/login">登录</a>
+        </div>
+      </div>
+      <div class="menu-box">
+        <div class="menu">
+          <a href="/" class="item active">主页</a>
+          <a href="/categories/tool" class="item">工具</a>
+          <a href="/categories/share" class="item">分享</a>
+          <a href="/categories/essay" class="item">随笔</a>
+          <a href="/categories/exhibition" class="item">展示</a>
+        </div>
+      </div>
+  </div>
+</div>
     <router-view></router-view>
   </div>
 </template>
@@ -12,12 +32,307 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section {
+  display: block;
+}
+
+ul,ol,li,p,img,h1,h2,h3,h4,h5,dt,dd,dl,body,span, strong,code, em, img, button, select, textarea {
+  color: #666;
+  padding: 0px;
+  margin: 0px;
+}
+h1,h2,h3,h4,h5,h6 {
+  font-weight: 100;
+}
+select{
+  padding: 2px;
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #f0f0f0;
+}
+ul, ol {
+  list-style: none;
+}
+
+img {
+  border: none;
+  vertical-align: top;
+}
+
+a {
+  text-decoration: none;
+  color: #333;
+}
+
+a:visited,a:link {
+  outline: none;
+}
+input{
+  padding: 2px 10px;
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #f0f0f0;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+.clear {
+  clear: both;
+}
+
+.clearfix:after {
+  display: block;
+  content: '';
+  height: 0;
+  clear: both;
+}
+
+body {
+  font-family: '微软雅黑';
+  line-height: 18px;
+  font-size: 14px;
+  color: #666;
+  background-color: #f9f9f9;
+}
+
+.fl{
+  float: left;
+}
+
+.fr{
+  float: right;
+}
+
+.font12{
+  font-size: 12px;
+  line-height: 18px;
+  color: #999;
+}
+
+.font14{
+  font-size: 14px;
+  line-height: 20px;
+  color: #999;
+}
+
+.font16{
+  font-size: 16px;
+  line-height: 22px;
+  color: #666;
+}
+
+.font18{
+  font-size: 18px;
+  line-height: 24px;
+  color: #666;
+}
+
+.font20{
+  font-size: 20px;
+  line-height: 26px;
+  color: #333
+}
+
+.font22{
+  font-size: 22px;
+  line-height: 28px;
+  color: #333
+}
+.font42{
+  font-size: 42px;
+  line-height: 84px;
+  color: #333
+}
+
+.iconfont{
+  font-family: 'iconfont-tcl';
+}
+
+.iconspan{
+  display: inline-block;
+  padding-right: 5px;
+}
+
+.margin5{
+  margin: 5px 0;
+}
+
+.margin10{
+  margin: 10px 0;
+}
+
+.marginT20{
+  margin-top: 20px;
+}
+
+
+.marginL12{
+  margin-left: 12px;
+}
+
+.clearfloat:after {
+  display: block;
+  clear: both;
+  content: "";
+  visibility: hidden;
+  height: 0
+}
+
+.clearfloat:before {
+  display: block;
+  clear: both;
+  content: "";
+  visibility: hidden;
+  height: 0
+}
+.width{
+  width: 100%;
+  max-width: 1120px;
+  min-width: 320px;
+  margin: 0 auto;
+  background-color: #FFF;
+  position: relative;
+}
+.head{
+  width: 100%;
+  padding: 0 60px;
+  height: 60px;
+  line-height: 60px;
+  background-color: #26272b;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+    position: relative;
+    z-index: 10;
+}
+.head.top {
+  height: 230px;
+}
+.head .width{
+  background-color: transparent;
+}
+.head .logo{
+    margin: 10px 0;
+    display: inline-block;
+    position: absolute;
+    left: 0;
+    bottom: 45px;
+    z-index: 10;
+}
+.head .logo-box {
+  padding: 5px 0;
+  height: 60px;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+}
+.head .logo img, .head .logo-box img {
+  max-height: 100%;
+}
+.head .menu-box{
+  width: 100%;
+  left: 0;
+  bottom: 0;
+  position: absolute;
+}
+.head.top .width{
+  height: 230px;
+}
+.head .menu-box .menu{
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+    max-width: 1120px;
+    padding: 0 15px;
+    background: #33363b;
+    z-index: 1;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -webkit-box-shadow: inset 0 1px 0 rgba(255,255,255,0.05), 0 -1px 0 rgba(0,0,0,0.2), inset 1px 0 0 rgba(0,0,0,0.2), inset -1px 0 0 rgba(0,0,0,0.2);
+    -webkit-box-shadow: inset 0 1px 0 rgba(255,255,255,0.05), 0 -1px 0 rgba(0,0,0,0.2), inset 1px 0 0 rgba(0,0,0,0.2), inset -1px 0 0 rgba(0,0,0,0.2);
+    box-shadow: inset 0 1px 0 rgba(255,255,255,0.05), 0 -1px 0 rgba(0,0,0,0.2), inset 1px 0 0 rgba(0,0,0,0.2), inset -1px 0 0 rgba(0,0,0,0.2);
+}
+.head .settings {
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  margin: 5px 0px 5px 10px;
+  background-color: #333;
+  display: inline-block;
+  vertical-align: top;
+  cursor: pointer;
+  top: 0;
+  right: 0;
+  position: absolute;
+}
+.head .settings>div {
+  width: 50px;
+    height: 50px;
+    border-radius: 50px;
+    background-color: #333;
+    -webkit-transition: all 0.5s;
+    transition: all 0.5s;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 1;
+    line-height: 50px;
+    font-size: 12px;
+    text-align: center;
+}
+.head .settings .raidus {
+  z-index: 2;
+  overflow: hidden;
+}
+.head .settings .raidus img{
+  max-width: 100%;
+  border-radius: 50px;
+  overflow: hidden;
+}
+.head .settings .trans {
+  height: 0px;
+  top: 15px;
+  overflow: hidden;
+}
+.head .settings:hover .trans {
+  border-radius: 10px;
+  height: 40px;
+  padding: 45px 0 15px 0;
+  background-color: #454545;
+}
+.head .settings .trans a{
+  display: block;
+  line-height: 22px;
+  color: #999;
+  -webkit-transition: all 0.3s ;
+  transition: all 0.3s ;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.head .settings .trans a:hover {
+  background-color: #3a3939;
+}
+.head .menu-box .menu-icon{
+  width: 60px;
+  height: 60px;
+  font-family: 'iconfont-tcl';
+  color: #FFF;
+  font-size: 26px;
+  cursor: pointer;
+  text-align: center;
+  display: none;
+  float: right;
+}
+.head .menu .item{
+  padding: 0 40px;
+  display: inline-block;
+  color: #FFF;
+  line-height: 50px;
+  margin-left: -4px;
+}
+.head .menu .item:hover{
+  color: #6bc30d;
+  /*background-color: #36333A;*/
+}
+.head .menu .item.active{
+  color: #6bc30d;
+  /*background-color: #302D35;*/
 }
 </style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,11 +1,228 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">nodegeek</a></li>
-     
-    </ul>
+    <article class="main">
+        
+
+<!-- centent html start -->
+<div class="backTop"></div>
+<div class="main width gray sizing">
+    <!-- centent left html start -->
+    <div class="width share main-left fl">
+        <h3 class="title sizing"><span class="border-l"></span>主页</h3>
+        <ul class="share-list blog">
+            
+        <li class="share-item">
+            <a href="/topic/58bfad19a53bef152ca54888"><div class="share-img"><img src="http://www.nodegeek.org/static/UXPAShare20170307/top.jpg"></div>
+            <div class="share-describe">
+                <div class="padd16">
+                    <div class="share-title">
+                         融合大用户体验之道 --- UXPA归来分享
+                    </div>
+                    <p>本次分享是基于设计部、产品部、酷友设计团队11月中下旬去苏州参加uxpa时工作坊学到的内容，收获满满</p>
+                </div>
+            </div></a>
+        </li>
+    
+        <li class="share-item">
+            <a href="/topic/58bfab41a53bef152ca54887"><div class="share-img"><img src="http://www.nodegeek.org/static/FlowerLonely20170308/top.jpg"></div>
+            <div class="share-describe">
+                <div class="padd16">
+                    <div class="share-title">
+                         岁月如花般寂寞
+                    </div>
+                    <p>风吹到哪里，就在哪个生根，任何地方，都可以生长，任何去处,都是归宿。</p>
+                </div>
+            </div></a>
+        </li>
+    
+        <li class="share-item">
+            <a href="/topic/58bf91c418339d49910b937a"><div class="share-img"><img src="http://www.nodegeek.org/static/SpeedUpLOGO20170307/top.jpg"></div>
+            <div class="share-describe">
+                <div class="padd16">
+                    <div class="share-title">
+                         速必达品牌LOGO优化设计过程全揭秘
+                    </div>
+                    <p>如果仔细想想我们正在经历的日常生活，就会发现LOGO无处不在。它能够起到品牌宣传、获取消费者认知的作用</p>
+                </div>
+            </div></a>
+        </li>
+    
+        <li class="share-item">
+            <a href="/topic/58bf8f8518339d49910b9379"><div class="share-img"><img src="http://www.nodegeek.org/static/blank20170307/top.jpg"></div>
+            <div class="share-describe">
+                <div class="padd16">
+                    <div class="share-title">
+                         空白页二三事
+                    </div>
+                    <p>空白页通常是指App或者网页上没有任何数据显示时所呈现的状态页面，通常在首次使用、用户主动清空数据或者出现错误</p>
+                </div>
+            </div></a>
+        </li>
+    
+        <li class="share-item">
+            <a href="/topic/58bf8c9a18339d49910b9378"><div class="share-img"><img src="http://www.nodegeek.org/static/UIUEJob20170307/top.jpg"></div>
+            <div class="share-describe">
+                <div class="padd16">
+                    <div class="share-title">
+                         UIUE工作日常-系统交互设计流程
+                    </div>
+                    <p>UIUE工作日常-系统交互设计流程 工作流程介绍</p>
+                </div>
+            </div></a>
+        </li>
+    
+        <li class="share-item">
+            <a href="/topic/58bf8b5218339d49910b9377"><div class="share-img"><img src="http://www.nodegeek.org/static/LOGODesignProcess20170307/top.jpg"></div>
+            <div class="share-describe">
+                <div class="padd16">
+                    <div class="share-title">
+                         TUX团队LOGO设计过程
+                    </div>
+                    <p>设计是为了解决问题而存在，那Logo也不例外。在设计前，我们应该先问一下自己，问一下需求方，为什么要设计这个Logo</p>
+                </div>
+            </div></a>
+        </li>
+    
+        <li class="share-item">
+            <a href="/topic/58becda718339d49910b9376"><div class="share-img"><img src="http://www.nodegeek.org/static/UIUEDesign20170307/top.jpg"></div>
+            <div class="share-describe">
+                <div class="padd16">
+                    <div class="share-title">
+                         UIUE设计部-TUX团队介绍
+                    </div>
+                    <p>TUX团队诞生于TCL集团 O2O事业部IT中心-UIUE设计部，是以不断提升组织核心自研电商系统及自控渠道的用户界面可用性优化与体验.</p>
+                </div>
+            </div></a>
+        </li>
+    
+        <li class="share-item">
+            <a href="/topic/58becaab18339d49910b9375"><div class="share-img"><img src="http://www.nodegeek.org/static/WebPorta20170307l/top.jpg"></div>
+            <div class="share-describe">
+                <div class="padd16">
+                    <div class="share-title">
+                         TUX设计规范网站三大入口ICON动画设计过程总结
+                    </div>
+                    <p>动画是网页上最活跃的元素，通常制作优秀、创意出众的动画是吸引浏览者的最有效的方法，不过现在的网页往往不是缺乏动画</p>
+                </div>
+            </div></a>
+        </li>
+    
+        <li class="share-item">
+            <a href="/topic/58bec28918339d49910b9374"><div class="share-img"><img src="http://www.nodegeek.org/static/UXDesignTrend20170307/top.jpg"></div>
+            <div class="share-describe">
+                <div class="padd16">
+                    <div class="share-title">
+                         2017年UX设计趋势
+                    </div>
+                    <p>2017年马上就要到了，我们回顾下今年用户体验都发生了什么事情，理解现状，展望一下17年用户体验设计将会有什么样的趋势。</p>
+                </div>
+            </div></a>
+        </li>
+    
+        <li class="share-item">
+            <a href="/topic/58b9613d6523421915dc0c93"><div class="share-img"><img src="http://www.nodegeek.org/static/weChattools20170303/Picture.jpg"></div>
+            <div class="share-describe">
+                <div class="padd16">
+                    <div class="share-title">
+                         微信头像定制工具
+                    </div>
+                    <p>节假日向来是是各大电商品牌的必争之日，在活动期间运用一些合理的推
+广手段，可以起到提升销售业绩、宣传品牌形象、展现品牌实力的目的。</p>
+                </div>
+            </div></a>
+        </li>
+            
+        </ul>
+
+        <!-- pages html start -->
+        <!--
+            <div class="page">
+                <ul class="page-list">
+                    <li class="page-item prev-page"></li>
+                    <li class="page-item active"><span>1</span></li>
+                    <li class="page-item"><a href="##">2</a></li>
+                    <li class="page-item"><a href="##">3</a></li>
+                    <li class="page-item"><a href="##">4</a></li>
+                    <li class="page-item"><span> ... </span></li>
+                    <li class="page-item next-page"><a href="##">下一页</a></li>
+                    <li class="page-item"><span>共 70 页</span></li>
+                </ul>
+            </div> -->
+        
+        <!-- pages html end -->
+    </div>
+    <!-- centent left html end -->
+
+    <!-- centent right html start -->
+    <div class="topic main-right">
+        <div class="container">
+            <h3 class="title sizing"><span class="border-l"></span>最新发现</h3>
+            <ul class="topic-list">
+                <li class="topic-item">
+                    <span class="radius font12">07-28</span>
+                    <a href="http://cnodejs.org/topic/588a959b1dc8ff8739cbc66d" class="font16"> Node.js 2016 回顾 </a>
+                </li>
+                <li class="topic-item">
+                    <span class="radius font12">06-08</span>
+                    <a href="http://cnodejs.org/topic/581b0c4ebb9452c9052e7acb" class="font16"> 《一起学 Node.js》彻底重写完毕 </a>
+                </li>
+                <li class="topic-item">
+                    <span class="radius font12">05-08</span>
+                    <a href="http://cnodejs.org/topic/57ea257b3670ca3f44c5beb6" class="font16"> 分享我用cnode社区api做微信小应用的入门过程 </a>
+                </li>
+                <li class="topic-item">
+                    <span class="radius font12">07-28</span>
+                    <a href="http://cnodejs.org/topic/565c4473d0bc14ae279399fe" class="font16"> 用Vue.js编写了cnodejs.org的h5客户端 </a>
+                </li>
+                <li class="topic-item">
+                    <span class="radius font12">07-28</span>
+                    <a href="http://cnodejs.org/topic/58ac640e7872ea0864fedf90" class="font16"> 《koa2进阶学习笔记》写了关于koa2入门教程 </a>
+                </li>
+                <li class="topic-item">
+                    <span class="radius font12">07-28</span>
+                    <a href="http://cnodejs.org/topic/58bd1f57de61e16b0a77179d" class="font16"> 基于 vue2 + vuex 构建一个具有 45 个页面 </a>
+                </li>
+                <li class="topic-item">
+                    <span class="radius font12">07-28</span>
+                    <a href="http://cnodejs.org/topic/56f3b0e8dd3dade17726fe85" class="font16"> Express 部署最佳实践-安全篇 </a>
+                </li>
+                <li class="topic-item">
+                    <span class="radius font12">07-28</span>
+                    <a href="http://cnodejs.org/topic/55c1fa095965fe2c74f477f7" class="font16"> Moa-api：快速开发api类后端的框架 </a>
+                </li>
+                <li class="topic-item">
+                    <span class="radius font12">06-08</span>
+                    <a href="http://cnodejs.org/topic/586823335eac96bb04d3e305" class="font16"> webpack代码分离 ensure 看了还不懂 </a>
+                </li>
+                <li class="topic-item">
+                    <span class="radius font12">05-08</span>
+                    <a href="http://cnodejs.org/topic/56ba030271204e03637a3870" class="font16"> 通过源码解析 Node.js 中导流（pipe）的实现 </a>
+                </li>
+            </ul>
+        </div>
+        
+        <div class="container marginT30">
+            <h3 class="title sizing"><span class="border-l"></span>工具库</h3>
+            <ul class="tool-list">
+                <li class="tool-item"><img src="http://www.nodegeek.org/ps.png"></li>
+                <li class="tool-item"><img src="http://www.nodegeek.org/dw.png"></li>
+                <li class="tool-item"><img src="http://www.nodegeek.org/ws.png"></li>
+                <li class="tool-item"><img src="http://www.nodegeek.org/sub.png"></li>
+                <li class="tool-item"><img src="http://www.nodegeek.org/np.png"></li>
+                <li class="tool-item"><img src="http://www.nodegeek.org/ar.png"></li>
+                <li class="tool-item"><img src="http://www.nodegeek.org/bc.png"></li>
+                <li class="tool-item"><img src="http://www.nodegeek.org/bk.png"></li>
+            </ul>
+        </div>
+
+    </div>
+    <!-- centent right html end -->
+    
+</div>
+
+<!-- centent html end -->
+
+    </article>
     
   </div>
 </template>
@@ -23,21 +240,224 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+/*  main style  */
+.main{
+  padding: 0 0 20px 0;
+  overflow: hidden;
+  position: relative;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+.main.gray {
+  background-color: #f0f0f0;
+  padding-bottom: 0;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+.main .backTop {
+  width: 100%;
+  height: 65px;
+  top: 0;
+  left: 0;
+  position: absolute;
+  background-color: #26272b;
+  z-index: 2;
 }
 
-a {
-  color: #42b983;
+.main .main-left{
+  width: 830px;
+  min-height: 1246px;
+  padding-left: 30px;
+  position: relative;
+  background-color: #FFF;
+  z-index: 3;
+  box-sizing: border-box;
+    -o-box-sizing: border-box;
+    -ms-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
 }
+
+.main .title{
+  padding-top: 20px;
+  font-weight: 400;
+}
+
+.main .item-l, .topic .item-l{
+  width: 220px;
+  height: 150px;
+}
+
+.main .item-l img, .topic .item-l img{
+  max-width: 100%;
+}
+
+.main .main-right {
+  margin-left: 830px;
+  padding-left: 20px;
+  background-color: #f0f0f0;
+  position: relative;
+  z-index: 3;
+}
+
+.main .topic-list{
+  width: 100%;
+  min-height: 168px;
+  overflow: hidden;
+}
+
+.main .topic-item{
+  padding: 20px 0;
+  overflow: hidden;
+}
+
+.main-left .topic-item{
+  border-bottom: 1px solid #f9f9f9;
+}
+
+.main-left .topic-item .classify {
+  color: #FFF;
+  padding: 0 14px;
+  background-color: #6bc30d;
+}
+
+.main .item-r, .topic .item-r{
+  margin-left: 240px;
+  position: relative;
+}
+.main .item-r .font18 {
+  height: 24px;
+  overflow: hidden;
+}
+
+.main .main-right .topic-item{
+  padding-bottom: 0;
+    height: 24px;
+    padding-right: 20px;
+}
+
+.main-right .topic-item .radius{
+  color: #FFF;
+  padding: 2px 8px;
+  border-radius: 14px;
+  /*background-color: #818D74;*/
+  background-color: #9FA599;
+  margin-right: 8px;
+}
+
+.main .topic .container{
+  width: 100%;
+  margin-bottom: 50px;
+}
+/*  2016-09-19 分享样式  */
+
+.padd16{
+  padding: 16px;
+}
+.share {
+  background-color: transparent;
+  overflow: hidden;
+}
+
+.share .title .border-l {
+    width: 3px;
+    height: 24px;
+    background-color: #6bc30d;
+    float: left;
+    margin-right: 10px;
+}
+.share-list {
+    width: 100%;
+}
+.share-list.blog {
+  margin-top: 30px;
+  overflow: hidden;
+}
+.share-item {
+    width: 31.4%;
+    float: left;
+    margin-bottom: 1.5%;
+    margin-right: 1.5%;
+    position: relative;
+}
+.blog .share-item {
+  width: 50%;
+  margin-right: 0;
+  padding-right: 30px;
+  box-sizing: border-box;
+  -o-box-sizing: border-box;
+  -ms-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+}
+.share-item img{
+  max-width: 100%;
+    vertical-align: bottom;
+}
+.share-describe {
+  height: 120px;
+    background: #fff;
+}
+.share-title {
+    font-size: 16px;
+    line-height: 24px;
+    color: #333;
+    height: 24px;
+    margin-top: -7px;
+    overflow: hidden;
+    margin-bottom: 10px;
+}
+.share-user {
+    height: 26px;
+    color: #999;
+    font-size: 14px;
+}
+.share-user-l {
+    float: left;
+    height: 26px;
+    width: 55%;
+    overflow: hidden;
+    white-space: nowrap;
+}
+.share-user-l img {
+    margin-right: 8px;
+    width: 26px;
+    height: 26px;
+    border-radius: 2px;
+    overflow: hidden;
+}
+.share-user img {
+    vertical-align: middle;
+}
+.share-user-r {
+    float: right;
+    margin-top: 4px;
+}
+.share-user-r div {
+    text-align: center;
+    color: #ddd;
+    float: left;
+    cursor: default;
+}
+.pinglun {
+    margin-left: 16px;
+}
+.listbox_pl{
+    font-size: 18px;
+    color: #ddd;
+    margin-right: 8px;
+}
+
+/*** tool-list style 2016920 ***/
+.tool-list {
+  overflow: hidden;
+}
+.tool-item {
+    width: 62px;
+    height: 62px;
+    margin: 12px;
+    float: left;
+}
+.tool-item img {
+  max-width: 100%;
+}
+
 </style>
